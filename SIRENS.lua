560c557626c74bac32108ec61ea74f8a4fd8204a
EastCoastModding <eastcoastmodding@gmail.com>
2021-03-27 23:01:04 -0400

    3.3.1 update

--- SIRENS.lua
+++ SIRENS.lua
@@ -29,20 +29,20 @@ SIREN TONE TABLE:
 ]]
 -- CHANGE SIREN NAMES, AUDIONAME, AUDIOREF
 SIRENS = {	
-	--[[1]]	  { Name = "Airhorn", 		String = "SIRENS_AIRHORN", 								Ref = 0 }, --1
-	--[[2]]	  { Name = "Wail", 			String = "VEHICLES_HORNS_SIREN_1", 						Ref = 0 }, --2
-	--[[3]]	  { Name = "Yelp", 			String = "VEHICLES_HORNS_SIREN_2", 						Ref = 0 }, --3
-	--[[4]]	  { Name = "Priority", 		String = "VEHICLES_HORNS_POLICE_WARNING", 				Ref = 0 }, --4
-	--[[5]]	  { Name = "Futura", 		String = "RESIDENT_VEHICLES_SIREN_WAIL_01", 			Ref = 0 }, --5
-	--[[6]]	  { Name = "Hetro", 		String = "RESIDENT_VEHICLES_SIREN_WAIL_02", 			Ref = 0 }, --6
-	--[[7]]	  { Name = "Sweep-1", 		String = "RESIDENT_VEHICLES_SIREN_WAIL_03", 			Ref = 0 }, --7
-	--[[8]]	  { Name = "Sweep-2", 		String = "RESIDENT_VEHICLES_SIREN_QUICK_01", 			Ref = 0 }, --8
-	--[[9]]	  { Name = "Hi-Low",		String = "RESIDENT_VEHICLES_SIREN_QUICK_02",			Ref = 0 }, --9
-	--[[10]]  { Name = "Whine Down",	String = "RESIDENT_VEHICLES_SIREN_QUICK_03", 			Ref = 0 }, --10
-	--[[11]]  { Name = "Powercall", 	String = "VEHICLES_HORNS_AMBULANCE_WARNING", 			Ref = 0 }, --11
-	--[[12]]  { Name = "Fire Horn", 	String = "VEHICLES_HORNS_FIRETRUCK_WARNING", 			Ref = 0 }, --12
-	--[[13]]  { Name = "Fire Yelp", 	String = "RESIDENT_VEHICLES_SIREN_FIRETRUCK_WAIL_01", 	Ref = 0 }, --13
-	--[[14]]  { Name = "Fire Wail", 	String = "RESIDENT_VEHICLES_SIREN_FIRETRUCK_QUICK_01", 	Ref = 0 }, --14
+	--[[1]]	  { Name = "Airhorn", 		 String = "SIRENS_AIRHORN", 								Ref = 0 }, --1
+	--[[2]]	  { Name = "Wail", 			 String = "VEHICLES_HORNS_SIREN_1", 						Ref = 0 }, --2
+	--[[3]]	  { Name = "Yelp", 			 String = "VEHICLES_HORNS_SIREN_2", 						Ref = 0 }, --3
+	--[[4]]	  { Name = "Priority", 		 String = "VEHICLES_HORNS_POLICE_WARNING", 				Ref = 0 }, --4
+	--[[5]]	  { Name = "Ambulance Wail", String = "RESIDENT_VEHICLES_SIREN_WAIL_01", 			Ref = 0 }, --5
+	--[[6]]	  { Name = "FIB Wail", 		 String = "RESIDENT_VEHICLES_SIREN_WAIL_02", 			Ref = 0 }, --6
+	--[[7]]	  { Name = "Motorcycle Wail", String = "RESIDENT_VEHICLES_SIREN_WAIL_03", 			Ref = 0 }, --7
+	--[[8]]	  { Name = "Ambulance Yelp",  String = "RESIDENT_VEHICLES_SIREN_QUICK_01", 			Ref = 0 }, --8
+	--[[9]]	  { Name = "FIB Yelp",		  String = "RESIDENT_VEHICLES_SIREN_QUICK_02",			Ref = 0 }, --9
+	--[[10]]  { Name = "Motorcycle Yelp", String = "RESIDENT_VEHICLES_SIREN_QUICK_03", 			Ref = 0 }, --10
+	--[[11]]  { Name = "Powercall", 	  String = "VEHICLES_HORNS_AMBULANCE_WARNING", 			Ref = 0 }, --11
+	--[[12]]  { Name = "Fire Horn", 	  String = "VEHICLES_HORNS_FIRETRUCK_WARNING", 			Ref = 0 }, --12
+	--[[13]]  { Name = "Fire Wail", 	  String = "RESIDENT_VEHICLES_SIREN_FIRETRUCK_WAIL_01", 	Ref = 0 }, --13
+	--[[14]]  { Name = "Fire Yelp", 	  String = "RESIDENT_VEHICLES_SIREN_FIRETRUCK_QUICK_01", 	Ref = 0 }, --14
 	}
 	
 	--ASSIGN SIRENS TO VEHICLES
@@ -61,4 +61,44 @@ SIRENS = {
 	['arrowrescue'] = { 12, 13, 14, 11 }, 
 	['eengine'] = { 12, 13, 14, 11 }, 
 	['RESCUE'] = { 12, 13, 14, 11 }, 
+	['sp1'] = { 1, 6, 9, 4 },
+	['sp2'] = { 1, 6, 9, 4 },
+	['sp3'] = { 1, 6, 9, 4 },
+	['sp4'] = { 1, 6, 9, 4 },
+	['sp5'] = { 1, 6, 9, 4 },
+	['sp6'] = { 1, 6, 9, 4 },
+	['sp7'] = { 1, 6, 9, 4 },
+	['sp8'] = { 1, 6, 9, 4 },
+	['sp9'] = { 1, 6, 9, 4 },
+	['sp10'] = { 1, 6, 9, 4 },
+	['sp11'] = { 1, 6, 9, 4 },
+	['sp12'] = { 1, 6, 9, 4 },
+	['sp13'] = { 1, 6, 9, 4 },
+	['sp14'] = { 1, 6, 9, 4 },
+	['sp15'] = { 1, 6, 9, 4 },
+	['sp16'] = { 1, 6, 9, 4 },
+	['sp17'] = { 1, 6, 9, 4 },
+	['sp18'] = { 1, 6, 9, 4 },
+	['sp19'] = { 1, 6, 9, 4 },
+	['sp20'] = { 1, 6, 9, 4 }, 
+	['sp21'] = { 1, 6, 9, 4 }, 
+	['sp22'] = { 1, 6, 9, 4 }, 
+	['sp23'] = { 1, 6, 9, 4 }, 
+	['sp24'] = { 1, 6, 9, 4 }, 
+	['sp25'] = { 1, 6, 9, 4 }, 
+	['sp26'] = { 1, 6, 9, 4 }, 
+	['sp27'] = { 1, 6, 9, 4 }, 
+	['sp28'] = { 1, 6, 9, 4 }, 
+	['umhp1'] = { 1, 7, 10, 4 }, 
+	['hp1'] = { 1, 7, 10, 4 }, 
+	['hp2'] = { 1, 7, 10, 4 }, 
+	['hp3'] = { 1, 7, 10, 4 }, 
+	['hp4'] = { 1, 7, 10, 4 }, 
+	['hp5'] = { 1, 7, 10, 4 }, 
+	['hp6'] = { 1, 7, 10, 4 }, 
+	['retrohp1'] = { 1, 7, 10, 4 }, 
+	['hpbike1'] = { 1, 7, 10, 4 }, 
+	['hpbike2'] = { 1, 7, 10, 4 }, 
+	['hp7'] = { 1, 7, 10, 4 }, 
+	['hp8'] = { 1, 7, 10, 4 },  
 	}
